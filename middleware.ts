c03274774461388c0c66f2d1996dde704e41de37
amerchel <ada.merchel98@gmail.com>
2025-06-29 08:56:28 +0200

    added DB

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,16 @@
-import { clerkMiddleware , createRouteMatcher} from '@clerk/nextjs/server';
+//export default clerkMiddleware(async (auth, req) => {
+//  if (isProtectedRoute(req)) await auth.protect()
+//}) przed export config
 
-const isProtectedRoute = createRouteMatcher(['/gracze'])
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect()
-})
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
